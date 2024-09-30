82e190eeacedffba5c73c6e96ddd9ce0ab14319e
AkylaiTopoeva <akylaytop@gmail.com>
2024-09-30 12:00:13 +0600

    add login and logout

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -19,3 +20,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
